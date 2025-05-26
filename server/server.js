6c9102d343ff314bb1f8bd1aa0f0460f5a855690
VitalieLozan <vitalielozan17@outlook.com>
2025-05-26 09:08:02 +0200

    new commit

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const server = jsonServer.create();
 const router = jsonServer.router('tasks.json');
 const middlewares = jsonServer.defaults();
 
-const allowedOrigin = 'https://todolist-app-hw2m.onrender.com';
+const allowedOrigin = 'http://localhost:3001';
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', allowedOrigin);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
@@ -25,7 +25,7 @@ server.use(jsonServer.bodyParser);
 
 server.post('/tasks', (req, res, next) => {
   const { taskName } = req.body;
-  if (!taskName || typeof taskName !== 'string' || taskName.length > 100) {
+  if (!taskName || typeof taskName !== 'string' || taskName.length > 50) {
     return res.status(400).json({ error: 'Task name is not valid.' });
   }
   next();
